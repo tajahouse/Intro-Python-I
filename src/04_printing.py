298b962cf246c681042c2f4bfc9b13f192311f67
Taja House <ahlliahtaja@yahoo.com>
2020-08-31 11:41:26 -0700

    Just starting

--- src/04_printing.py
+++ src/04_printing.py
@@ -1,9 +1,3 @@
-"""
-Python provides a number of ways to perform printing. Research
-how to print using the printf operator, the `format` string 
-method, and by using f-strings.
-"""
-
 x = 10
 y = 2.24552
 z = "I like turtles!"
@@ -11,7 +5,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+txt = '{x}, {y}, {z}'
+print(txt.format(x=x, y=y, z=z))
 # Use the 'format' string method to print the same thing
-
+print(f'{x}, {y}, {z}')
 # Finally, print the same thing using an f-string
