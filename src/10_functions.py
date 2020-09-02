8918f05c3db83b00e662c66db28e378c8e65e3ed
Taja House <ahlliahtaja@yahoo.com>
2020-09-02 09:14:56 -0700

    completed 11

--- src/10_functions.py
+++ src/10_functions.py
@@ -7,9 +7,9 @@
 
 def is_even(num):
     if num % 2 == 0:
-        print("even") 
+        return "EVEN!"
     else:
-        print("odd") 
+        return "odd"
 num = input("Enter a number: ")
 num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
